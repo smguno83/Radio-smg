45aafdfdd6db125b4f725ac42d0e59b7e00dc432
smguno83 <matiasgutierrez402@gmail.com>
2025-10-29 12:18:07 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,19 +3,35 @@ const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon.png'
+  './icon.png',
+  './icon512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then((response) => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Para los streams de radio, no cachear (solo el HTML y assets)
+  if (event.request.url.includes('.aac') ||
+      event.request.url.includes('.mp3') ||
+      event.request.url.includes('stream')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
